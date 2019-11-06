a71a5f96689a45c748390f2d83ec9962ae1ae6e4
richa <richa@Desktop-Phann>
2019-11-06 08:35:15 -0800

    Adding Small map 3x3

---
+++ BattleOfTheCentury/src/Map.java
@@ -0,0 +1,32 @@
+
+public class Map {
+
+	int[] [] floorMap = new int [3][3]; {
+	//[row] [column]
+	//row 1
+		floorMap [0] [0] = 1;
+		floorMap [0] [1] = 1;
+		floorMap [0] [2] = 1;
+	//row 2
+		floorMap [1] [0] = 1;
+		floorMap [1] [1] = 1;
+		floorMap [1] [2] = 1;
+	//row 3
+		floorMap [2] [0] = 1;
+		floorMap [2] [1] = 1;
+		floorMap [2] [2] = 1;
+		
+		floorMap [0] [0] = 1;
+		floorMap [0] [0] = 1;
+		floorMap [0] [0] = 1;
+		floorMap [0] [0] = 1;
+		floorMap [0] [0] = 1;
+		floorMap [0] [0] = 1;
+	}
+			
+			
+	
+	
+	
+	
+}
