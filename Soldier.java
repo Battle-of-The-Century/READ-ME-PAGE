29734989b2b39d5497993c4850223411b852aa3b
tvue0 <55511729+tvue0@users.noreply.github.com>
2019-11-25 23:10:34 -0600

    Add files via upload

---
+++ Soldier.java
@@ -0,0 +1,20 @@
+package Project;
+
+public class Soldier extends Unit{
+	private String name = "Soldier";
+	
+	public Soldier(int healthPoints, int attackPoints, int defencePoints, int movementSpeed) {
+		super(healthPoints, attackPoints, defencePoints, movementSpeed);
+		
+	}
+	
+	@Override
+	public String toString() {
+		String soldier = name + " HP: " + super.getHealthPoints()
+							  + " Attack: " + super.getAttackPoints()
+							  + " Defence: " + super.getDefencePoint()
+							  + " Speed: " + super.getMovementSpeed();
+		return soldier;
+	}
+
+}
