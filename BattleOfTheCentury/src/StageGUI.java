6717cd02d1a75f51590e4662e752b4b159e53cae
qm4383jc <47437784+qm4383jc@users.noreply.github.com>
2019-12-05 23:02:11 -0800

    jpg lines added
    need to figure out how to make it work

--- BattleOfTheCentury/src/StageGUI.java
+++ BattleOfTheCentury/src/StageGUI.java
@@ -12,64 +12,70 @@ import javax.swing.*;
 
 
 public class StageGUI extends JFrame{
-	
 
-	
+
+
 	private JButton attackBtn = new JButton("Attack");
 	private JButton moveBtn = new JButton("Move");
-	
+
 	private JButton map = new JButton("Map");
-	private JButton[][] squares = new JButton[7][7];
-	
+	private JButton[][] squares = new JButton[8][8];
+
 	//Colors:
-	private Color colorBlack = Color.BLACK; 
-	
+	private Color colorBlack = Color.BLACK;
+
 	//Images:
-	//private ImageIcon *variable ex. pawn* = new ImageIcon("white pawn.jpg");
-	
+	private ImageIcon Hero = new ImageIcon("Hero.jpg");
+	private ImageIcon Tank = new ImageIcon("Tank.jpg");
+	private ImageIcon Soldier = new ImageIcon("Soldier.jpg");
+
+
+
+
+
 	private JLabel selectLbl = new JLabel("Select Unit");
 	private JLabel chooseLbl = new JLabel("Actions");
 	private JTextArea historyTxtArea = new JTextArea();
-	
-	
-	
+
+
+
 	//public GridLayoutManager() {
 		//super("GUI GridLayout Manager - (click a valid square to move piece)")
-		
-		
-		
+
+
+
 	//}
-	
+
 	Hero hero = new Hero(25, 10, 25, 3);
 	Soldier soldier = new Soldier(15, 5, 15, 5);
 	Tank tank = new Tank(35, 7, 35, 1);
 	private Army[] units = {soldier, tank, hero};
 	private JComboBox unit = new JComboBox(units);
-	
+
 	//Map panel
 	private JPanel firstPanel = new JPanel();
-	
+
 	//Move & attack button
 	private JPanel secondPanel1 = new JPanel(new GridLayout(1,2));
-	
+
 	//Unit Jcombo box
 	private JPanel secondPanel2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-	
+
 	//display text box or history
 	private JPanel secondPanel3 = new JPanel(new FlowLayout(FlowLayout.CENTER));
-	
-	
+
+
 	private JPanel rightHelperPanel = new JPanel(new GridLayout(3,1));
 
 	public StageGUI(String title) {
 		super(title = "Battle of The Century");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new GridLayout(1, 2));
-		// min width is 600 
+		// min width is 600
 		setSize(600, 400);
-		
+
 		setResizable(false);
-		
+
 		createFirstPanel();
 		createSecondPanel1();
 		createSecondPanel2();
@@ -82,26 +88,19 @@ public class StageGUI extends JFrame{
 
 	//for squares for map
 		private Container contents;
-	
+
 	private void createFirstPanel() {
-		//contents.setLayout(new GridLayout(6,6));
-		firstPanel.add(map);
-		
-		contents = getContentPane();
-		//contents.setLayout(new FlowLayout(FlowLayout.RIGHT));
-		
-		contents.setLayout(new GridLayout(6,6));
-		
-		for (int i = 0; i < 6; i++)
+
+		for (int i = 0; i < 7; i++)
 		{
-			for(int j = 0; j < 6; j++)
+			for(int j = 0; j < 7; j++)
 			{
 				squares[i][j] = new JButton();
 				if ((i + j) % 2 != 0)
 				{
 					squares[i][j].setBackground(colorBlack);
 				}
-				contents.add(squares[i][j]);
+				firstPanel.add(squares[i][j]);
 				//squares[i][j].addActionListener(l);
 			}
 		}
@@ -111,9 +110,9 @@ public class StageGUI extends JFrame{
 		secondPanel1.add(moveBtn);
 		//secondPanel1.add(chooseLbl, BorderLayout.CENTER);
 		secondPanel1.add(attackBtn);
-		
+
 	}
-	
+
 	private void createSecondPanel2() {
 		secondPanel2.add(selectLbl);
 		secondPanel2.add(unit);
@@ -122,7 +121,7 @@ public class StageGUI extends JFrame{
 		secondPanel3.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "History"));
 		secondPanel3.add(historyTxtArea);
 	}
-	
+
 	private void createRightHelperPanel() {
 		rightHelperPanel.add(secondPanel1);
 		rightHelperPanel.add(secondPanel2);
@@ -133,14 +132,14 @@ public class StageGUI extends JFrame{
 		add(firstPanel);
 		//add();
 		add(rightHelperPanel);
-		
+
 	}
 
-	
-	
+
+
 	public static void main(String[] args) {
 		StageGUI game = new StageGUI("ok");
 	}
-	
-	
+
+
 }
